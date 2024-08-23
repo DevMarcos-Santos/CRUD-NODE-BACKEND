adc9d4633d96bfd18ad7e4de5b65a7e03807e351
DevMarcos-Santos <boicemarcosvinicius@gmail.com>
2024-08-23 14:53:20 -0300

    add: "methods read, update and delete"

--- src/controller/personController.ts
+++ src/controller/personController.ts
@@ -1,17 +1,60 @@
-import { Body, Controller, Post, Res } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Post,
+  Put,
+  Res,
+} from '@nestjs/common';
 import { PersonService } from 'src/service/personService';
 import { Response } from 'express';
 import { Person } from 'src/types/types';
 
-@Controller()
+@Controller('persons')
 export class PersonController {
   constructor(private personService: PersonService) {}
 
   @Post('create')
   async create(@Body() body: Person, @Res() res: Response) {
     try {
-      const result = await this.personService.create(body.name, body.email);
+      const result = await this.personService.create(body);
       return res.status(201).json(result);
+    } catch (error) {
+      return res.status(400).send(error);
+    }
+  }
+
+  @Get('')
+  async read(@Res() res: Response) {
+    try {
+      const result = await this.personService.read();
+      return res.status(200).json(result);
+    } catch (error) {
+      return res.status(400).send(error);
+    }
+  }
+
+  @Put('update/:id')
+  async update(
+    @Param('id') id: string,
+    @Body() body: Person,
+    @Res() res: Response,
+  ) {
+    try {
+      const result = await this.personService.update(id, body);
+      return res.status(200).json(result);
+    } catch (error) {
+      res.status(400).send(error);
+    }
+  }
+
+  @Delete('delete/:id')
+  async delete(@Param('id') id: string, @Res() res: Response) {
+    try {
+      const result = await this.personService.delete(id);
+      return res.status(200).send(result);
     } catch (error) {
       return res.status(400).send(error);
     }
--- src/service/personService.ts
+++ src/service/personService.ts
@@ -1,16 +1,44 @@
 import { Injectable } from '@nestjs/common';
 import { PersonRepository } from 'src/repository/personRepository';
+import { Person } from 'src/types/types';
 
 @Injectable()
 export class PersonService {
   constructor(private personRepository: PersonRepository) {}
 
-  async create(name: string, email: string) {
+  async create(person: Person) {
     try {
-      const result = await this.personRepository.createPerson(name, email);
+      const result = await this.personRepository.createPerson(person);
       return result;
     } catch (error) {
       throw error.message;
+    }
+  }
+
+  async read() {
+    try {
+      const result = await this.personRepository.readPersons();
+      return result;
+    } catch (error) {
+      throw error;
+    }
+  }
+
+  async update(id: string, person: Person) {
+    try {
+      const result = await this.personRepository.updatePerson(id, person);
+      return result;
+    } catch (error) {
+      throw error.message;
+    }
+  }
+
+  async delete(id: string) {
+    try {
+      const result = await this.personRepository.deletePerson(id);
+      return result;
+    } catch (error) {
+      throw error;
     }
   }
 }
